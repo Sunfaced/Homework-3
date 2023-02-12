30324dbb8d94c05de3492562c70f02f81bb46112
Alexander <akoceryzhkin@gmail.com>
2023-02-12 12:55:50 +0300

    First Commit

---
+++ main.js
@@ -0,0 +1,76 @@
+'use strict';
+
+// Пример 1
+
+// function dogYears(dogName, age){
+    //     let years = age * 2;
+    //     console.log(dogName + ' is ' + years + ' years old');
+    // }
+    // let myDog = 'Rex';
+    // dogYears(myDog, 4);
+    
+    // function makeTea (cups, tea){
+    //     console.log('Breawing ' + cups + ' cups of' + tea);
+    // }
+    // let guests = 2;
+    // makeTea(guests, 'Earl Grey');
+    
+    // function secret(){
+    //     console.log('The secret of life is 28');
+    // }
+    // secret();
+
+// Переменные: years, myDog, guests
+// Функции: dogYears, makeTea, secret
+// Аргументы: (myDog, 4), (guests, 'Earl Grey')
+// Параметры: (dogName, age), (cups, tea)
+
+// Пример 2
+
+// let txt = 'JS';
+// console.log(txt.toLowerCase());
+
+// Пример 3 
+
+// let txt = 'я люблю javascript';
+// console.log(txt.substr(2,16));
+
+// let txt = 'я люблю javascript';
+// console.log(txt.substring(2,18));
+
+// let txt = 'я люблю javascript';
+// console.log(txt.slice(2,18));
+
+// Пример 4 
+
+// let txt = 'я люблю javascript';
+// console.log(txt.search('люблю'));
+
+// Пример 5
+
+// let txt = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos quaerat similique mollitia';
+// let n = 15;
+// let result = txt;
+// if(txt.length > n){
+//     result=txt.substring(0,n);
+//     console.log(result + '...');
+// }else{
+//     (txt.length <= n);
+//     result=txt;
+//     console.log(result);
+// }
+
+// Пример 6
+
+// let txt = 'Я-люблю-JS!';
+// console.log(txt.replace(/-/g,'!'));
+
+// Пример 7
+
+// let txt = 'я люблю JS';
+// console.log(txt.split(' '));
+
+// Пример 8
+
+// let txt = 'привет мир';
+// console.log(txt.split(''));
